47a7841d38474cafc1cd51ca2d0f8b11ea15a829
IvoryMoth <julien_owen@outlook.com>
2026-06-13 02:23:09 -0700

    all this jade spear and still no head

---
+++ src/main/java/noelle/taylor/Item/ModItems.java
@@ -0,0 +1,30 @@
+package noelle.taylor.Item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import noelle.taylor.TaylorSDreamModpack;
+
+public class ModItems {
+
+    //jade stuff idk if more will be added
+    public static Item JADE_SPEAR_HEAD = registerItem("jade_spear_head",
+            new Item(new Item.Settings().maxCount(1)));
+
+    //nether gold
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TaylorSDreamModpack.MOD_ID, name),item);
+    }
+
+    public static void registerModItems() {
+        TaylorSDreamModpack.LOGGER.info("Registering Mod Items for" + TaylorSDreamModpack.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(JADE_SPEAR_HEAD);
+        });
+    }
+}
--- src/main/java/noelle/taylor/TaylorSDreamModpack.java
+++ src/main/java/noelle/taylor/TaylorSDreamModpack.java
@@ -2,6 +2,7 @@ package noelle.taylor;
 
 import net.fabricmc.api.ModInitializer;
 
+import noelle.taylor.Item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,9 +16,7 @@ public class TaylorSDreamModpack implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
